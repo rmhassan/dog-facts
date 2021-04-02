7967bb9b45a0a8cbe7f79bb3249436d832313747
Hassan Raja <connect.rmhassan@gmail.com>
2021-04-02 17:44:00 +0500

    publish on github pages, reduce header padding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-blue-500 p-16 sm:p-24 shadow-xl">
+  <div class="bg-blue-500 p-4 sm:p-8 shadow-xl">
     <h1 class="font-bold text-md sm:text-5xl text-white">Dog Facts</h1>
   </div>
 </template>
