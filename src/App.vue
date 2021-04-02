3bfa3f1e4d6d2fbfc6502675841eb42848301765
Hassan Raja <connect.rmhassan@gmail.com>
2021-04-02 15:00:00 +0500

    header, tailwindcss, font

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Open Sans", sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-blue-500 p-16 sm:p-24 shadow-xl">
+    <h1 class="font-bold text-md sm:text-5xl text-white">Dog Facts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
